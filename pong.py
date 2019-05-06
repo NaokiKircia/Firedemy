b62743233becd01a355a0c24f3063e26b6880ac5
Aleksandra <naoki.kircia@gmail.com>
2019-05-06 20:28:07 +0200

    works fine to canvas declaration, no errors

--- pong.py
+++ pong.py
@@ -1,17 +1,19 @@
 # PONG pygame from https://gist.github.com/vinothpandian/4337527
 
 import _random
-import pygame, sys
-from pygame.locals import *
+import pygame
+import sys
+from pygame import *
+from easygui import *
 
 pygame.init()
 fps = pygame.time.Clock()
 
 #colors from RGB Color Codes Chart
-WHITE = (255,255,255)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
 
 #globals
 
@@ -20,26 +22,29 @@ HEIGHT = 400
 BALL_RADIUS = 20
 PAD_WIDTH = 8
 PAD_HEIGHT = 80
-HALF_PAD_WIDTH = PAD_WIDTH / 2
-HALF_PAD_HEIGHT = PAD_HEIGHT /2
-ball_pos = [0,0]
-bal_vel = [0,0]
+HALF_PAD_WIDTH = PAD_WIDTH // 2
+HALF_PAD_HEIGHT = PAD_HEIGHT // 2
+ball_pos = [0, 0]
+bal_vel = [0, 0]
 paddle1_vel = 0
 paddle2_vel = 0
 l_score = 0
 r_score = 0
 
 #canvas declaration
-window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)  #skąd liczby po przecinku? w docs pygame ich nie ma
-pygame.dislay.set_caption("Hello World")                  #zmiana nazwy okna
+# skąd liczby po przecinku? w docs pygame ich nie ma
+window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+# zmiana nazwy okna
+pygame.display.set_caption("Hello World")
 
 #helper function that spawns a ball, returns a position vector and a velocity vector
 #if right is True, spawn to the right, else spawn to the left
-def ball_init(right):
-    global ball_pos, ball_vel # these are vectors stored as lists
-    ball_pos = [WIDTH/2, HEIGHT/2]
-    horz = random.randrange(2,4)
-    vert = random.randrange(1,3)
+def ball_init (right):
+    # these are vectors stored as lists
+    global ball_pos, ball_vel
+    ball_pos = [WIDTH//2, HEIGHT//2]
+    horz = random.randrange(2, 4)
+    vert = random.randrange(1, 3)
 
     if right == False:
         horz = - horz
@@ -47,3 +52,5 @@ def ball_init(right):
     ball_vel = [horz, -vert]
 
 #define event handlers
+def init():
+    global paddle1_pos, paddle2_pos, paddle1_ve1, paddle2_ve2
