3726e0411e714cc9ddc9fb73b0a08b447b8632c5
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2019-05-07 10:04:06 +0200

    define event handlers

--- pong.py
+++ pong.py
@@ -32,7 +32,7 @@ l_score = 0
 r_score = 0
 
 #canvas declaration
-# skąd liczby po przecinku? w docs pygame ich nie ma
+# skad liczby po przecinku? w docs pygame ich nie ma
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 # zmiana nazwy okna
 pygame.display.set_caption("Hello World")
@@ -53,4 +53,26 @@ def ball_init (right):
 
 #define event handlers
 def init():
-    global paddle1_pos, paddle2_pos, paddle1_ve1, paddle2_ve2
+    #these are floats
+    global paddle1_pos, paddle2_pos, paddle1_ve1, paddle2_ve2,l_score, r_score
+    #these are int
+    global score1, score2
+    paddle1_pos = [HALF_PAD_WIDTH - 1, HEIGHT // 2]
+    l_score = 0
+    r_score = 0
+    if random.randrage(0, 2) == 0:
+        ball_init(True)
+    else:
+        ball_init(False)
+
+#draw function of canvas
+def draw(canvas):
+    global paddle1_pos, paddle2_pos, ball_poss, ball_vel, l_score, r_score
+
+    canvas.fill(BLACK)
+    pygame.draw.line(canvas, WHITE, [WIDTH // 2, 0], [WIDTH//2, HEIGHT], 1)
+    pygame.draw.line(canvas, WHITE, [PAD_WIDTH, 0], [PAD_WIDTH, HEIGHT], 1)
+    pygame.draw.line(canvas, WHITE, [WIDTH - PAD_WIDTH, 0], [WIDTH - PAD_WIDTH, HEIGHT], 1)
+    pygame.draw.circle(canvas, WHITE, [WIDTH // 2, HEIGHT // 2], 70, 1)
+
+    #update paddle's vertical position, kep paddle on the screen
